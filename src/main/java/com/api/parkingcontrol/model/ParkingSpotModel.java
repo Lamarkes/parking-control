edb9034c6b22b245db170154c5c13112641471cb
Lamark <lamark12ricarte@gmail.com>
2023-10-29 12:07:01 -0300

    First commit

---
+++ src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -0,0 +1,15 @@
+package com.api.parkingcontrol.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUUID = 1L;
+
+
+
+}
