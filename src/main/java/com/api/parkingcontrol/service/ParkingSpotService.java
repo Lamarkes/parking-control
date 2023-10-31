7d4a6fa0f8a64cf06efbb54267be91a0e5e3d718
Lamark <lamark12ricarte@gmail.com>
2023-10-31 11:45:25 -0300

    new feat: findAll, findOne, deÃlete

--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -5,6 +5,10 @@ import com.api.parkingcontrol.repository.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class ParkingSpotService {
 
@@ -17,6 +21,19 @@ public class ParkingSpotService {
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
         return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public List<ParkingSpotModel> findAll(){
+        return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id){
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel){
+        parkingSpotRepository.delete(parkingSpotModel);
     }
 
     public boolean existsByLicensePlateCar(String licensePlateCar){
